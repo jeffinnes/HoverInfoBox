f01405da102860950a4200577298cd9733e308ca
Jeffery Innes <jinnes@tucson.local>
2017-06-20 11:45:29 -0700

    Replace Dialog box "On click" functionality with "Hover/Cursor Tracking"

--- res/js/main.js
+++ res/js/main.js
@@ -1,5 +1,45 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  //Hide dialog boxes
+  $("#TUC-AV-01").dialog({
+    autoOpen: false
+  });
+  $("#TUC-DC-10").dialog({
+    autoOpen: false
+  });
 
-  
+  //Box functionality
+  //hover version
+  $("#av01").hover(function () {
+      $("#TUC-AV-01").dialog('open');
+    }, function () {
+      $("#TUC-AV-01").dialog('close');
+    }).mousemove(function (mouse) {
+      $("#TUC-AV-01").dialog({dialogClass: "no-close", position: {
+        my: "left+10 bottom+100%",
+        of: mouse,
+        collision: "fit"}
+      });
+    });
 
-});
+    $("#dc10").hover(function () {
+        $("#TUC-DC-10").dialog('open');
+      }, function () {
+        $("#TUC-DC-10").dialog('close');
+      }).mousemove(function (mouse) {
+        $("#TUC-DC-10").dialog({dialogClass: "no-close", position: {
+          my: "left+10 bottom+100%",
+          of: mouse,
+          collision: "fit"}
+        });
+      });
+
+    //on click version
+    /*$( "#av01" ).click(function() {
+      $( "#TUC-AV-01" ).dialog( "open" );
+    });
+
+    $( "#dc10" ).click(function() {
+      $( "#TUC-DC-10" ).dialog( "open" );
+    });*/
+
+  });//End doc.ready
